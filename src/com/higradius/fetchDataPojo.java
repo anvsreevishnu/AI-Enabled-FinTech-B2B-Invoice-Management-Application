22f4977a704b96d16d83c7d34e2bff1b1d5fd10b
anvsreevishnu <anvsvishnu@gmail.com>
2021-07-27 00:05:39 +0530

    First commit

---
+++ src/com/higradius/fetchDataPojo.java
@@ -0,0 +1,58 @@
+package com.higradius;
+
+public class fetchDataPojo {
+	
+	private String name_customer;
+	private String cust_number;
+	private String invoice_id;
+	private String total_open_amount;
+	private String due_in_date_1;
+	private String delay_predicted;
+	private String notes;
+	
+	
+	public String getName_customer() {
+		return name_customer;
+	}
+	public void setName_customer(String name_customer) {
+		this.name_customer = name_customer;
+	}
+	public String getCust_number() {
+		return cust_number;
+	}
+	public void setCust_number(String cust_number) {
+		this.cust_number = cust_number;
+	}
+	public String getInvoice_id() {
+		return invoice_id;
+	}
+	public void setInvoice_id(String invoice_id) {
+		this.invoice_id = invoice_id;
+	}
+	public String getTotal_open_amount() {
+		return total_open_amount;
+	}
+	public void setTotal_open_amount(String total_open_amount) {
+		this.total_open_amount = total_open_amount;
+	}
+	public String getDue_in_date_1() {
+		return due_in_date_1;
+	}
+	public void setDue_in_date_1(String due_in_date_1) {
+		this.due_in_date_1 = due_in_date_1;
+	}
+	public String getDelay_predicted() {
+		return delay_predicted;
+	}
+	public void setDelay_predicted(String delay_predicted) {
+		this.delay_predicted = delay_predicted;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
+
+}
